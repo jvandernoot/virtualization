873309990d7a531ec45a3ac959cbe5dad93b5132
Jamie VanDernoot <james.vandernoot@slalom.com>
2021-08-16 10:20:57 -0500

    Moved vagrant and multipass scripts here.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # vagrant-boxes
+
 Collection of vagrant boxes.
+
+# Bash Profile
+
+Add the following lines to your `~/.bash_profile` file to load aliases for the following categories:
+
+## Defaults
+
+```bash
+source <working-folder>/vagrant-boxes/vagrant.sh
+```
---
+++ vagrant.sh
@@ -0,0 +1,19 @@
+
+CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "$DIR/../includes/setup.sh"
+
+
+# Kali Linux
+alias kali-cd="cd $CURRENT_DIR/kali-linux"
+alias kali-status="cd $CURRENT_DIR/kali-linux && vagrant status"
+alias kali-up="cd $CURRENT_DIR/kali-linux && vagrant up"
+alias kali-ssh="cd $CURRENT_DIR/kali-linux && vagrant ssh"
+
+### Multipass - Linux: https://multipass.run/ ###
+alias mp='multipass'
+
+# Use a function to delete and purge an instance since aliases don't accept parameters
+mp-purge() {
+    echo "Deleting and purging Multipass instance: $1"
+    multipass delete "$1" && multipass purge && multipass list
+}
